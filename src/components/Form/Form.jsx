6de713acadc34b0f81d45374bc57cad34caf8fb8
Dmitry <d.ramanchuk@arateg.com>
2021-09-23 09:46:07 +0300

    First

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import { Button } from "../Button";
+
+function getId() {
+  return (
+    "_" + Math.random().toString(36).substr(2, 9)
+  );
+}
+
+export function Form({ onClick }) {
+  const [text, setText] = useState("");
+
+  const onChangeInput = (event) => {
+    setText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    console.log("onClickAdd Form");
+
+    const todoshka = {
+      text: text,
+      checked: false,
+      id: getId(),
+    };
+
+    onClick(todoshka);
+
+    setText("");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <input
+        style={{ fontSize: 100 }}
+        value={text}
+        onChange={onChangeInput}
+        placeholder="Сделай же что-нибудь!"
+      />
+      <Button text="Add" onClick={onClickAdd} />
+    </div>
+  );
+}
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Form } from "../Form";
+
+export class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+  }
+
+  onClickAdd = (todoshka) => {
+    console.log("onClickAdd TodoList");
+
+    const newTodos = [
+      ...this.state.todos,
+      todoshka,
+    ];
+
+    this.setState({ todos: newTodos });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onClick={this.onClickAdd} />
+        {this.state.todos.map((item) => {
+          return (
+            <div key={item.id}>
+              <p style={{ fontSize: 50 }}>
+                {item.text}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
