5bc680b1d25f03d73b326ad1616a12c7e6bf009f
Nastyshka14 <nastya.korshun.1997@mail.ru>
2021-10-18 01:18:31 +0300

    privetiki

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import "./App.css";
-import { TodoList } from "./components/TodoList";
+// import { useState, createContext } from "react";
+// import { Provider, useSelector, useDispatch } from "react-redux";
+import { SliderContainer } from "./components/Slider";
+import styles from "./App.css";
+import { RootRouter } from "./router/RootRouter";
+// import { store } from "./redux/store";
+// import { RootRouter } from "./router/RootRouter";
 
 function App() {
   return (
-    <div className="App">
-      <TodoList />
+    <div className={styles.App}>
+      <RootRouter />
+      <SliderContainer />
     </div>
   );
 }
